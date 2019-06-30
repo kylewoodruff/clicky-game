3e671514e709c31ff187d2e7116e632e1fd1970d
Kyle Woodruff <kylewoodruff@gmail.com>
2019-06-30 01:35:55 -0700

    Final touches

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -5,8 +5,8 @@ function Jumbotron(props) {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container-fluid">
-                <h1 className="display-4">Star Wars Clicky Game</h1>
-                <p className="lead">Test your memory out with this game. Try to not click on the same image twice or you loose.</p>
+                <h1 className="display-4">Star Wars Memory Game</h1>
+                <p className="lead">Test your memory out with this game. Do not click on the same image twice or you loose.</p>
                 <hr className="my-4"></hr>
                 <div className="score">
                     <h4>Score: {props.score}</h4>
